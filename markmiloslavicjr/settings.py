ff0fbb050c76104d7c89aacf6cd8c8275bc88a16
Espresso4TheMind <miloslavicma@gmail.com>
2019-08-05 16:07:48 -0400

    another push

--- markmiloslavicjr/settings.py
+++ markmiloslavicjr/settings.py
@@ -84,15 +84,15 @@ WSGI_APPLICATION = 'markmiloslavicjr.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     },
-    'psql': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'markmiloslavicjr',
         'USER': 'postgres',
-        'PASSWORD': 'password',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
     }
