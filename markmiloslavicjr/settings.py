bc08c79c76449b571888ff1ad178eb8d405021c8
Espresso4TheMind <miloslavicma@gmail.com>
2019-08-16 12:17:40 -0400

    Removing unnecessary key and txt files

--- markmiloslavicjr/settings.py
+++ markmiloslavicjr/settings.py
@@ -3,8 +3,6 @@ import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-with open('./secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
 SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
