9f79b734f18db2894da990f5b162377ff3a0058d
Espresso4TheMind <miloslavicma@gmail.com>
2019-08-19 14:50:01 -0400

    Updated resume Changed Hobbies and Interests Attempting to fix SECRET_KEY issue

--- markmiloslavicjr/settings.py
+++ markmiloslavicjr/settings.py
@@ -3,12 +3,17 @@ import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-with open('./secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
-SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY)
+# with open('./secret_key.txt') as f:
+#     SECRET_KEY = f.read().strip()
+# SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY)
+if open('./secret_key.txt') is True:
+    with open('./secret_key.txt') as f:
+        SECRET_KEY = f.read().strip()
+else:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DJANGO_DEBUG', 1)))
+
 SECURE_SSL_REDIRECT = bool(int(os.environ.get('SECURE_SSL_REDIRECT', 0)))
 
 ALLOWED_HOSTS = [
